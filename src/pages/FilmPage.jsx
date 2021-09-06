c9a1641f133784375df36ae704a737067f4dca1f
frida stenberg <fridastenberg97@hotmail.com>
2021-09-06 17:30:50 +0200

    updated all pages

--- src/pages/FilmPage.jsx
+++ src/pages/FilmPage.jsx
@@ -1,7 +1,57 @@
+import { getPosts } from "../services/StarWarsAPI";
+import { useQuery } from "react-query";
+import Button from "react-bootstrap/Button";
+import { useState, useEffect } from "react"
+
 const FilmPage = () => {
+    const [page, setPage] = useState(1);
+
+    const { data, error, isError, isLoading, isPreviousData } = useQuery(
+        ["films", page],
+        () => getPosts("films", page),
+        {
+            staleTime: 1000 * 60 * 5, // 5 mins
+            cacheTime: 1000 * 60 * 30, // 30 mins
+            keepPreviousData: true, // keep previous data
+        }
+    )
+
+    useEffect(() => {
+        console.log("data", data)
+    }, [data])
+
     return(
         <div>
             <h1>FilmPage</h1>
+            {!data && <></>}
+
+            {isLoading && <p>Loading...</p>}
+
+            {isError && <p>{error}</p>}
+
+            {data && (
+                data.starWarsData.map((film, i) => (
+                    <p key={i}>{film.title}</p>
+                ))
+            )}
+
+            <div>
+                <Button onClick={() => setPage(
+                    (currentPage) => Math.max(currentPage - 1, 1))} disabled={page === 1}>
+                    Previous Page
+                </Button>
+                <span>Current Page: {page}</span>
+
+                <Button onClick={() => {
+                    if (!isPreviousData && data.results.next) {
+                        setPage((currentPage) => currentPage + 1);
+                    }
+                }}
+                    // Disable the Next Page button until we know a next page is available
+                    disabled={isPreviousData || !data?.results.next}>
+                    Next Page
+                </Button>
+            </div>
         </div>
     )
 }
--- src/pages/PlanetPage.jsx
+++ src/pages/PlanetPage.jsx
@@ -1,7 +1,57 @@
+import { getPosts } from "../services/StarWarsAPI";
+import { useQuery } from "react-query";
+import Button from "react-bootstrap/Button";
+import { useState, useEffect } from "react"
+
 const PlanetPage = () => {
+    const [page, setPage] = useState(1);
+
+    const { data, error, isError, isLoading, isPreviousData } = useQuery(
+        ["planets", page],
+        () => getPosts("planets", page),
+        {
+            staleTime: 1000 * 60 * 5, // 5 mins
+            cacheTime: 1000 * 60 * 30, // 30 mins
+            keepPreviousData: true, // keep previous data
+        }
+    )
+
+    useEffect(() => {
+        console.log("data", data)
+    }, [data])
+
     return(
         <div>
             <h1>PlanetPage</h1>
+            {!data && <></>}
+
+            {isLoading && <p>Loading...</p>}
+
+            {isError && <p>{error}</p>}
+
+            {data && (
+                data.starWarsData.map((planet, i) => (
+                    <p key={i}>{planet.name}</p>
+                ))
+            )}
+
+            <div>
+                <Button onClick={() => setPage(
+                    (currentPage) => Math.max(currentPage - 1, 1))} disabled={page === 1}>
+                    Previous Page
+                </Button>
+                <span>Current Page: {page}</span>
+
+                <Button onClick={() => {
+                    if (!isPreviousData && data.results.next) {
+                        setPage((currentPage) => currentPage + 1);
+                    }
+                }}
+                    // Disable the Next Page button until we know a next page is available
+                    disabled={isPreviousData || !data?.results.next}>
+                    Next Page
+                </Button>
+            </div>
         </div>
     )
 }
