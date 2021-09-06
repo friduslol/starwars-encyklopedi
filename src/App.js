0a07b5383976751349db73910e600137b65f1131
frida stenberg <fridastenberg97@hotmail.com>
2021-09-06 16:32:13 +0200

    fetching categories from api, looping data in navbar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import HomePage from "./pages/Homepage";
 import NavBar from "./components/Navbar"
 import { BrowserRouter, Route } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-      <NavBar />
-      <Route exact path="/" component={HomePage} />
+        <QueryClientProvider client={queryClient}>
+          <NavBar />
+          <ReactQueryDevtools initialIsOpen={false} />
+          <Route exact path="/" component={HomePage} />
+        </QueryClientProvider>
       </BrowserRouter>
     </div>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,18 @@
 import { NavLink } from "react-router-dom";
+import { useQuery } from "react-query";
+import { fetchCategory } from "../services/CategoryAPI";
 
 const Navbar = () => {
+    const { data } = useQuery("category", fetchCategory);
     return(
         <div>
             <NavLink to="/">Home</NavLink>
+            {data &&
+                Object.getOwnPropertyNames(data).map((name, i) => (
+                    <NavLink to={name} key={i}>
+                        {name.toLocaleUpperCase()}
+                    </NavLink>
+            ))}
         </div>
     )
 };
