877f32ebbd7f32f73c95731a0a7d4f6bce23557b
frida stenberg <fridastenberg97@hotmail.com>
2021-09-06 16:18:45 +0200

    created home P and navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import HomePage from "./pages/Homepage";
+import NavBar from "./components/Navbar"
+import { BrowserRouter, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+      <NavBar />
+      <Route exact path="/" component={HomePage} />
+      </BrowserRouter>
     </div>
   );
 }
